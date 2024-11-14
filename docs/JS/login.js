385d96234b1d941dda689389ed3a905c874d5fb0
GabrielFraga <gabrielbff2006@gmail.com>
2024-11-14 16:23:53 -0300

    updateLogin

--- docs/JS/login.js
+++ docs/JS/login.js
@@ -2,15 +2,16 @@ var  email = document.querySelector('#em')
 var password = document.querySelector('#pass')
 
 
-function login(){
+function login(event){
+    event.preventDefault()
     let insertedEmail =  email.value
     let insertedPassword = password.value
     console.log(insertedEmail)
     console.log(insertedPassword)
 
-    if(insertedEmail == 'admin@gmail.com' && insertedPassword == '1234'){
+    if(insertedEmail === 'admin@gmail.com' && insertedPassword === '1234'){
 
-        window.location.replace('//gab1203.github.io/Forum/')
+        window.location.replace('index.html')
     }else{
         alert('Email ou senha incorreto!')
     }
