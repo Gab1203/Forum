ed732e1e29f183192df674ecec8d0565395f60f0
GabrielFraga <gabrielbff2006@gmail.com>
2024-11-18 07:21:03 -0300

    login

--- docs/JS/login.js
+++ docs/JS/login.js
@@ -19,41 +19,43 @@
 
 // }
 
-// function register(event){
-//     event.preventDefault()
-   
-//     var mail = document.getElementById('mail')
-//     var senha = document.getElementById('senha')
-//     var nome = document.getElementById('nome')
-
-//     alert('Cadastro realizado!')
-// }
 
 var email = document.querySelector('#em');
 var password = document.querySelector('#pass');
 
 
-var mail = "usuario@exemplo.com";
-var senha = "12345";
+
+function register(event){
+    event.preventDefault()
+  
+    var mail = document.getElementById('mail')
+    var senha = document.getElementById('senha')
+    var nome = document.getElementById('nome')
+
+    alert('Cadastro realizado!')
+}
+
 
 
 function login(event) {
     
     event.preventDefault();
-
     
-    let insertedEmail = email.value.trim();
+    let insertedEmail = email.value.trim().toLowerCase();
     let insertedPassword = password.value.trim();
 
     
     console.log(insertedEmail);
     console.log(insertedPassword);
+    
+    console.log(mail.value);
+    console.log(senha.value);
 
-    if (insertedEmail === mail && insertedPassword === senha) {
+    if (insertedEmail === mail.value.toLowerCase() && insertedPassword === senha.value) {
         window.location.replace('index.html');
     } else {
         alert('Email ou senha incorreto!');
     }
 }
 
-document.getElementById('loginBtn').addEventListener('click', login);
+
