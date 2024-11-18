46b39d685f993a46168e45d6a0dbd7156834e193
fernandomec <fernandomec2013@gmail.com>
2024-11-18 06:48:29 -0300

    login funcional

--- docs/JS/login.js
+++ docs/JS/login.js
@@ -1,30 +1,59 @@
-var  email = document.querySelector('#em')
-var password = document.querySelector('#pass')
+// var email = document.querySelector('#em')
+// var password = document.querySelector('#pass')
 
+// var mail = "usuario@exemplo.com"
+// var senha = "12345"
 
+// function login(event){
+//     event.preventDefault()
+//     let insertedEmail =  email.value
+//     let insertedPassword = password.value
+//     console.log(insertedEmail)
+//     console.log(insertedPassword)
 
-function login(event){
-    event.preventDefault()
-    let insertedEmail =  email.value
-    let insertedPassword = password.value
-    console.log(insertedEmail)
-    console.log(insertedPassword)
+//     if(insertedEmail === mail && insertedPassword === senha){
+//         window.location.replace('index.html');
+//     } else {
+//         alert('Email ou senha incorreto!');
+//     }
 
-    if(insertedEmail === mail.value && insertedPassword === senha.value){
+// }
 
-        window.location.replace('index.html')
-    }else{
-        alert('Email ou senha incorreto!')
-    }
-
-}
-
-function register(event){
-    event.preventDefault()
+// function register(event){
+//     event.preventDefault()
    
-    var mail = document.getElementById('')
-    var senha = document.getElementById('')
-    var nome = document.getElementById('')
+//     var mail = document.getElementById('mail')
+//     var senha = document.getElementById('senha')
+//     var nome = document.getElementById('nome')
 
-    alert('Cadastro realizado!')
+//     alert('Cadastro realizado!')
+// }
+
+var email = document.querySelector('#em');
+var password = document.querySelector('#pass');
+
+
+var mail = "usuario@exemplo.com";
+var senha = "12345";
+
+
+function login(event) {
+    
+    event.preventDefault();
+
+    
+    let insertedEmail = email.value.trim();
+    let insertedPassword = password.value.trim();
+
+    
+    console.log(insertedEmail);
+    console.log(insertedPassword);
+
+    if (insertedEmail === mail && insertedPassword === senha) {
+        window.location.replace('index.html');
+    } else {
+        alert('Email ou senha incorreto!');
+    }
 }
+
+document.getElementById('loginBtn').addEventListener('click', login);
